e8c1fec75a98aed198bee375ce61c1ca4399d3b6
SANDESH-code <chandhugadda27@gmail.com>
2025-01-01 19:12:35 +0530

    init

---
+++ work.py
@@ -0,0 +1,26 @@
+print('''
+   python - matplotib
+   python - numpy 
+   python - modules (knowledge)
+   certification>>>nptl
+      
+
+2.cloud aws(certifications)
+2 year 2 sem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      ''')
